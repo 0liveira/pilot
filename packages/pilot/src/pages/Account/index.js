20c0758798ec7de6303c48e984f9f49f9043b4dc
rsmelo <rodrigosm87@gmail.com>
2019-10-08 19:54:57 -0300

    account: add async imports

--- packages/pilot/src/pages/Account/index.js
+++ packages/pilot/src/pages/Account/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import PropTypes from 'prop-types'
 import {
   Redirect,
@@ -15,20 +15,48 @@ import {
 import { translate } from 'react-i18next'
 
 import Account from '../../containers/Account'
-import Login from './Login'
 import Logo from '../../components/Logo'
-import PasswordRecovery from './PasswordRecovery/Request'
-import PasswordRecoveryConfirmation from './PasswordRecovery/Request/Confirmation'
-import PasswordReset from './PasswordRecovery/Reset'
-import PasswordResetConfirmation from './PasswordRecovery/Reset/Confirmation'
 import Presentation from './Presentation'
-import CompanySignup from './SignUp/Company'
-import CompanySignupConfirmation from './SignUp/Company/Confirmation'
-import UserSignupConfirmation from './SignUp/User/Confirmation'
-import UserSignUp from './SignUp/User'
 import TestLogo from '../../components/Logo/TestLogo'
+import Loader from '../../components/Loader'
 
 import environment from '../../environment'
+
+const Login = lazy(() => import(
+  /* webpackChunkName: "login" */ './Login'
+))
+
+const PasswordRecovery = lazy(() => import(
+  /* webpackChunkName: "password-recovery" */ './PasswordRecovery/Request'
+))
+
+const PasswordRecoveryConfirmation = lazy(() => import(
+  /* webpackChunkName: "password-recovery-confirmation" */ './PasswordRecovery/Request/Confirmation'
+))
+
+const PasswordReset = lazy(() => import(
+  /* webpackChunkName: "password-reset" */ './PasswordRecovery/Reset'
+))
+
+const PasswordResetConfirmation = lazy(() => import(
+  /* webpackChunkName: "password-reset-confirmation" */ './PasswordRecovery/Reset/Confirmation'
+))
+
+const CompanySignup = lazy(() => import(
+  /* webpackChunkName: "company-signup" */ './SignUp/Company'
+))
+
+const CompanySignupConfirmation = lazy(() => import(
+  /* webpackChunkName: "company-signup-confirmation" */ './SignUp/Company/Confirmation'
+))
+
+const UserSignUp = lazy(() => import(
+  /* webpackChunkName: "user-signup" */ './SignUp/User'
+))
+
+const UserSignupConfirmation = lazy(() => import(
+  /* webpackChunkName: "user-signup-confirmation" */ './SignUp/User/Confirmation'
+))
 
 const DARK_BASE = 'dark'
 const LIGHT_BASE = 'light'
@@ -54,69 +82,70 @@ const enhance = compose(
 const AccountArea = ({ history: { location }, t }) => {
   const base = getBaseByPath(location.pathname)
   return (
-    <Account
-      t={t}
-      logo={getEnvironmentLogo()}
-      base={base}
-      primaryContent={(
-        <Switch>
-          <Route
-            path="/account/login"
-            render={() => <Login base={base} />}
-          />
-          <Route
-            path="/account/password/recovery/confirmation"
-            component={PasswordRecoveryConfirmation}
-          />
-          <Route
-            path="/account/password/recovery"
-            render={() => <PasswordRecovery base={base} />}
-          />
-          <Route
-            path="/account/password/reset/confirmation"
-            component={PasswordResetConfirmation}
-          />
-          <Route
-            path="/account/password/reset/:token"
-            render={() => <PasswordReset base={base} />}
-          />
-          <Route
-            path="/account/password/reset/confirmation"
-            component={PasswordResetConfirmation}
-          />
-          <Route
-            path="/account/password/reset/:token"
-            component={() => <PasswordReset base={base} />}
-          />
-          <Route
-            path="/account/signup/confirmation"
-            render={() => <CompanySignupConfirmation />}
-          />
-          <Route
-            path="/account/signup/invite/confirmation"
-            render={() => <UserSignupConfirmation />}
-          />
-          <Route
-            path="/account/signup/invite"
-            render={() => <UserSignUp base={base} />}
-          />
-          <Route
-            path="/account/signup"
-            render={() => <CompanySignup base={base} />}
-          />
-          <Redirect to="/account/login" />
-        </Switch>
+    <Suspense
+      fallback={(
+        <Loader
+          text={t('loading')}
+          visible
+        />
       )}
-      secondaryContent={(
-        <Switch>
-          <Route
-            path="/account"
-            component={Presentation}
-          />
-          <Redirect to="/account/login" />
-        </Switch>
-      )}
-    />
+    >
+      <Account
+        t={t}
+        logo={getEnvironmentLogo()}
+        base={base}
+        primaryContent={(
+          <Switch>
+            <Route
+              path="/account/login"
+              render={() => <Login base={base} />}
+            />
+            <Route
+              path="/account/password/recovery/confirmation"
+              component={() => <PasswordRecoveryConfirmation />}
+            />
+            <Route
+              path="/account/password/recovery"
+              render={() => <PasswordRecovery base={base} />}
+            />
+            <Route
+              path="/account/password/reset/confirmation"
+              component={() => <PasswordResetConfirmation />}
+            />
+            <Route
+              path="/account/password/reset/:token"
+              render={() => <PasswordReset base={base} />}
+            />
+            <Route
+              path="/account/signup/invite/confirmation"
+              render={() => <UserSignupConfirmation />}
+            />
+            <Route
+              path="/account/signup/invite"
+              render={() => <UserSignUp base={base} />}
+            />
+            <Route
+              path="/account/signup/confirmation"
+              render={() => <CompanySignupConfirmation />}
+            />
+            <Route
+              path="/account/signup"
+              render={() => <CompanySignup base={base} />}
+            />
+            <Redirect to="/account/login" />
+          </Switch>
+        )}
+        secondaryContent={(
+          <Switch>
+            <Route
+              path="/account"
+              component={Presentation}
+            />
+            <Redirect to="/account/login" />
+          </Switch>
+        )}
+      />
+    </Suspense>
   )
 }
 
